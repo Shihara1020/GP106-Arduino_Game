d9926c0ff41d19ddc109a8b891c9f5f9cd6ed100
Shihara Dewagedara <153976318+Shihara1020@users.noreply.github.com>
2024-07-23 16:50:30 +0530

    Update GUL.py

--- GUL.py
+++ GUL.py
@@ -5,8 +5,8 @@ import time
 import random
 from tkinter import messagebox
 board=Arduino ("COM3")
-#===============================================Definied the led pin,button and pizzo================================
-#Indicate the choice of compute using 5 five led
+#===============================================Define the LED pin, button, and piezo================================
+#Indicate the choice of the computer using 5 five LEDs
 Rock=board.digital[2]
 Paper=board.digital[3]
 Scissors=board.digital[4]
@@ -16,7 +16,7 @@ for led in [Rock,Paper,Scissors,Lizard,spock]:
     led.mode=OUTPUT
 #--------------------------------------------------------------------------------------------
 
-#Indicate the score of user
+#Indicate the score of the user
 Score1=board.digital[7]
 Score2=board.digital[8]
 Score3=board.digital[9]
@@ -25,18 +25,18 @@ for score in [Score1,Score2,Score3,Score4]:
     score.mode=OUTPUT
 #------------------------------------------------------------------------------------------
 
-#Indicate the End/start/Round
+#Indicate the End/Start round
 Start_end_Bulb=board.digital[11]
 Start_end_Bulb.mode=OUTPUT
 #-----------------------------------------------------------------------------------------
 
-#piezzor buzzer
+#piezo buzzer
 piezzor=board.digital[12]
 piezzor.mode=OUTPUT
 
 #------------------------------------------------------------------------------------------
 
-#switch for choices
+#switches for choices
 switch_rock=board.analog[4]
 switch_paper=board.analog[3]
 switch_scissors=board.analog[2]
@@ -58,10 +58,10 @@ it.start()
 #======================================================Who is winner function========================================
 def who_is_winner(user_select_element):
     global round,Computer_score,User_score,selected
-    #choise of compuetr 
+    #choice of computer 
     selected=["Rock","Paper","Scissors","Lizard","Spock"]
     computer_select_element=random.choice(selected)
-    #Indicate the bulb(computer chioice)
+    #Indicate the bulb(computer choice)
     computer_leds={
         "Rock":Rock,
         "Paper":Paper,
@@ -90,7 +90,7 @@ def who_is_winner(user_select_element):
         }
         if user_select_element in win_conditions[computer_select_element]:
             Computer_score+=1
-            result="Computer win"
+            result="Computer"
             winner="Computer"
         else:
             User_score+=1
@@ -98,16 +98,16 @@ def who_is_winner(user_select_element):
             winner="Player"
     
     #add choices for the table(GUI) 
-    Game_page_lb.config(text=f"Computer chose: { computer_select_element}\nYour chose:{user_select_element}\n{result}")
+    Game_page_lb.config(text=f"Computer chose: { computer_select_element}\nYou chose:{user_select_element}\n{result}")
     table.insert('',tk.END, values=(round, user_select_element,computer_select_element, winner),)
     root.update()
     Score(User_score,Computer_score)
 #=========================================================================================================
 
 
-#====================================diplay the user score and computer score=============================
+#====================================display the user score and computer score=============================
 def Score(User_score,Computer_score):
-    #Turn off the all leds
+    #Turn off all the LEDs
     for score in [Score1,Score2,Score3,Score4]:
         score.write(0)
 
@@ -119,7 +119,7 @@ def Score(User_score,Computer_score):
     elif User_score==3:
         Score1.write(1)
         Score2.write(1)
-    #display the coumputer score
+    #display the computer score
     if Computer_score==1:
         Score3.write(1)
     elif Computer_score==2:
@@ -136,7 +136,7 @@ def Score(User_score,Computer_score):
 #=================================Go next round function========================================
 def Go_nextt_round():
     global round,start_time
-    #blink the 5 leds
+    #blink the 5 LEDs
     for _ in range(5):
         Start_end_Bulb.write(0)
         time.sleep(0.5)
@@ -154,7 +154,7 @@ def Go_nextt_round():
     start_time=time.time()
 #=============================================waiting function===============================
 def waiting():
-    Game_page_lb.config(text="You can choise now...")
+    Game_page_lb.config(text="You can choose now...")
     rocklabel.config(state="disabled")
     paperlabel.config(state="disabled")
     scissorslabel.config(state="disabled")
@@ -165,7 +165,6 @@ def waiting():
 #========================================END the game==========================================
 def End_the_game():
     global Computer_score,User_score,indicate_state
-    #print("Restart the game")
     Game_page_lb.config(text="Game End")
     rocklabel.config(state="disabled")
     paperlabel.config(state="disabled")
@@ -176,7 +175,7 @@ def End_the_game():
     #turn off the start button
     Start_end_Bulb.write(0)
     indicate_state=False
-    #blink the 5 leds
+    #blink the 5 LEDs
     for _ in range(10):
         for led in [Rock,Paper,Scissors,Lizard,spock]:
             led.write(0)
@@ -184,13 +183,13 @@ def End_the_game():
         for led in [Rock,Paper,Scissors,Lizard,spock]:
             led.write(1)
         time.sleep(0.5)
-    #Display the massege who is winner    
+    #Display the message saying who is the winner    
     if Computer_score==User_score:
-        messagebox.showinfo("who is winner","Good game the game is tie")
+        messagebox.showinfo("Who is the winner","Good game, the game is a tie")
     elif Computer_score<User_score:
-        messagebox.showinfo("who is winner","Good game the game is win you  happy")
+        messagebox.showinfo("Who is the winner","Good game, the game is won by you. Congratulations!")
     elif Computer_score>User_score:
-        messagebox.showinfo("who is winner","Hard luck the game is lost")
+        messagebox.showinfo("Who is the winner","Hard luck, the game is lost")
     #turn off the bulb
     for score in [Score1,Score2,Score3,Score4]:
         score.write(0)
@@ -201,7 +200,7 @@ def End_the_game():
 # Function to reset the game
 def reset_game():
     global Computer_score,User_score,round
-    round=1
+    round=0
     Computer_score= 0
     User_score = 0
     Game_page_lb.config(text="Please press the start button to start the game")
@@ -216,6 +215,16 @@ def buzz(duration):
     time.sleep(duration)
     piezzor.write(0)
 
+#check function
+def check():
+    global Computer_score,User_score,round
+    if Computer_score==4 or User_score==4:
+        buzz(0.8)
+        End_the_game()
+    elif round ==7:
+        buzz(0.8)
+        End_the_game()
+
 #================================start the game=========================
 def start_the_game():
     global round,indicate_state,Computer_score,User_score,start_time,selected
@@ -223,41 +232,35 @@ def start_the_game():
     indicate_state=False
     Computer_score=0
     User_score=0
-    round=1
 
     while True:
         root.update()
-        if Computer_score==4 or User_score==4:
-            buzz(0.8)
-            End_the_game()
-        if round ==8:
-            buzz(0.8)
-            End_the_game()
-        #read the start end switch
+        #read the start/end switch
         button_state = Start_end_switch.read()
         time.sleep(0.1)
 
         if button_state==True:
-            if indicate_state==True:#the led is one-->the game is end
-                #The game  is end
+            if indicate_state==True:
+                #The game has ended
                 buzz(0.8)
                 End_the_game()
             else:
-                #The game is start
+                #The game has started
                 indicate_state=True
                 Start_end_Bulb.write(1)
                 #variable define
                 time.sleep(0.2)
                 Game_page_lb.configure(text="The Game is begining...")
+                round=1
                 Round.config(text="Round 1")
                 root.update()
-                time.sleep(5)
+                time.sleep(3)
                 waiting()
-                #first round time is begin
+                #first round time begins
                 start_time=time.time()
                 
         if indicate_state==True:
-            if time.time()-start_time<=30:
+            if time.time()-start_time<=5:
                 a=switch_lizzard.read()
                 b=switch_paper.read()
                 c=switch_rock.read()
@@ -266,66 +269,75 @@ def start_the_game():
                 if a is not None and b is not None and c is not None and d is not None and e is not None:
                     if a>0.5:
                         Lizardlabel.config(state="normal")
-                        Game_page_lb.config(text="Please waiting to computer choice the item..")
+                        Game_page_lb.config(text="Please wait for the computer to choose the item..")
                         root.update()
                         Lizard.write(1)
                         time.sleep(0.5)
                         Lizard.write(0)
                         
-                        time.sleep(5)
+                        time.sleep(1)
                         who_is_winner("Lizard")
-                        Go_nextt_round()
-
+                        check()
+                        if round!=0:
+                            Go_nextt_round()
                     elif b>0.5:
                         paperlabel.config(state="normal")
-                        Game_page_lb.config(text="Please waiting to computer choice the item..")
+                        Game_page_lb.config(text="Please wait for the computer to choose the item..")
                         root.update()
 
                         Paper.write(1)
                         time.sleep(0.5)
                         Paper.write(0)
 
-                        time.sleep(5)
+                        time.sleep(1)
                         who_is_winner("Paper")
-                        Go_nextt_round()
+                        check()
+                        if round!=0:
+                            Go_nextt_round()
 
                     elif c>0.5:
                         rocklabel.config(state="normal")
-                        Game_page_lb.config(text="Please waiting to computer choice the item..")
+                        Game_page_lb.config(text="Please wait for the computer to choose the item..")
                         root.update()
 
                         Rock.write(1)
                         time.sleep(0.5)
                         Rock.write(0)
 
-                        time.sleep(5)
+                        time.sleep(1)
                         who_is_winner("Rock")
-                        Go_nextt_round()
+                        check()
+                        if round!=0:
+                            Go_nextt_round()
 
                     elif d>0.5:
                         scissorslabel.config(state="normal")
-                        Game_page_lb.config(text="Please waiting to computer choice the item..")
+                        Game_page_lb.config(text="Please wait for the computer to choose the item..")
                         root.update()
                         Scissors.write(1)
                         time.sleep(0.5)
                         Scissors.write(0)
 
-                        time.sleep(5)
+                        time.sleep(1)
                         who_is_winner("Scissors")
-                        Go_nextt_round()
+                        check()
+                        if round!=0:
+                            Go_nextt_round()
 
                     elif e>0.5:
                         Spocklabel.config(state="normal")
-                        Game_page_lb.config(text="Please waiting to computer choice the item..")
+                        Game_page_lb.config(text="Please wait for the computer to choose the item..")
                         root.update()
 
                         spock.write(1)
                         time.sleep(0.5)
                         spock.write(0)
 
-                        time.sleep(5)
+                        time.sleep(1)
                         who_is_winner("Spock")
-                        Go_nextt_round()
+                        check()
+                        if round!=0:
+                            Go_nextt_round()
             else:
                 #the time is passed
                 Start_end_Bulb.write(0)
@@ -333,20 +345,21 @@ def start_the_game():
                 Computer_score+=1
                 #inside the table
                 computer_select_element=random.choice(selected)
-                Game_page_lb.config(text=f"Computer chose: { computer_select_element}\nYour chose:Time has passed\nComputer win")
-                table.insert('',tk.END, values=(round, "Time has passed",computer_select_element,"Computer win"),)
+                Game_page_lb.config(text=f"Computer chose: { computer_select_element}\nYou chose:Time has passed\nComputer wins")
+                table.insert('',tk.END, values=(round, "Time has passed",computer_select_element,"Computer"),)
                 root.update()
 
                 Score(User_score,Computer_score)
-                time.sleep(5)
-                Go_nextt_round()
-                Start_end_Bulb.write(1)
-
+                time.sleep(1)
+                check()
+                if round!=0:
+                    Go_nextt_round()
+                    Start_end_Bulb.write(1)
         time.sleep(0.2)
 
 #=========================================================GUI======================================================== 
 root = tk.Tk()
-#size of the user inter face
+#size of the user interface
 root.geometry("900x900")
 root.title("Mini Game")
 root.resizable(False,False)
@@ -355,10 +368,8 @@ root.configure(bg="#f0f0f0")
 #Round number
 round_count=1
 #Title of the game
-title = tk.Label(root, text="Scissors Paper Rock Spock Lizard Game", font=("Arial", 15), bg="#0097e8", fg="white")
+title = tk.Label(root, text="Rock Paper Scissors Lizard Spock Game", font=("Arial", 15), bg="#0097e8", fg="white")
 title.pack()
-#space=tk.label(root,text="")
-#space.pack()
 #display the round
 Round=tk.Label(root, text='The Game is starting', fg="#0097e8", font=("Arial", 13))
 Round.pack(pady=20)
